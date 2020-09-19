6e11eef69ddfa839a8293e67db627739aeed488c
RavenBP <ravenbpowless@gmail.com>
2020-09-19 14:29:11 -0400

    added scrolling background

---
+++ SpaceShooterGame/Assets/Backgrounds/Scripts/Scroll.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour
+{
+    Material material;
+    Vector2 offset;
+
+    public int xVelocity;
+    public int yVelocity;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Assigning offset variable
+        offset = new Vector2(xVelocity, yVelocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Updating material offset
+        material.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
