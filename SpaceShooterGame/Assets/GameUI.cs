3d92067ef06e5bce5f1d69c97dd22db72c6f395a
RavenBP <ravenbpowless@gmail.com>
2020-09-19 20:32:00 -0400

    added more assets + altered scenes
    - added music/sfx assets
    - added pause button to GameScene
    - made small various changes to scenes

---
+++ SpaceShooterGame/Assets/GameUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUI : MonoBehaviour
+{
+    private bool gamePaused;
+
+    public void PauseGame()
+    {
+        if (gamePaused == false)
+        {
+            Time.timeScale = 0;
+            gamePaused = true;
+        }
+        else if (gamePaused == true)
+        {
+            Time.timeScale = 1;
+            gamePaused = false;
+        }
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        // Toggle visibility of text?
+        // Change to a different scene?
+    }
+}
