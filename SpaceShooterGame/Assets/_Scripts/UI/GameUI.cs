f109c7c74f04f661825d59dbd5bcf2b9b59426ab
RavenBP <ravenbpowless@gmail.com>
2020-09-22 22:30:07 -0400

    Asset management
    - Added functionality to credits/controls
    - Added/changed visual assets throughout all scenes
    - Removed assets that will likely not be used
    - Changed several position and colour values to various texts and buttons for consistency
    - Merged previous UI scripts into singular UI script component
    - Changed folder layout

---
+++ SpaceShooterGame/Assets/_Scripts/UI/GameUI.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameUI : MonoBehaviour
+{
+    public GameObject basePanel; // Used for main UI
+    public GameObject overlayPanel1; // Used for first optional panel
+    public GameObject overlayPanel2; // Used for second optional panel
+
+    private bool creditsDisplayed;
+    private bool controlsDisplayed;
+    private bool gamePaused;
+
+    ////////////////////////////////////// Used by MainMenuScene
+    public void PlayGame() // Also used by GameOverScene
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ControlsButton()
+    {
+        if (controlsDisplayed == false)
+        {
+            controlsDisplayed = true;
+            basePanel.SetActive(false);
+            overlayPanel1.SetActive(true);
+        }
+        else if (controlsDisplayed == true)
+        {
+            controlsDisplayed = false;
+            overlayPanel1.SetActive(false);
+            basePanel.SetActive(true);
+        }
+    }
+
+    public void CreditsButton()
+    {
+        if (creditsDisplayed == false)
+        {
+            creditsDisplayed = true;
+            basePanel.SetActive(false);
+            overlayPanel2.SetActive(true);
+        }
+        else if (creditsDisplayed == true)
+        {
+            creditsDisplayed = false;
+            overlayPanel2.SetActive(false);
+            basePanel.SetActive(true);
+        }
+    }
+
+    ////////////////////////////////////// Used by GameScene
+    public void PauseGame()
+    {
+        if (gamePaused == false)
+        {
+            Time.timeScale = 0;
+            gamePaused = true;
+            basePanel.SetActive(false);
+            overlayPanel1.SetActive(true);
+        }
+        else if (gamePaused == true)
+        {
+            Time.timeScale = 1;
+            gamePaused = false;
+            overlayPanel1.SetActive(false);
+            basePanel.SetActive(true);
+        }
+    }
+
+    public void ReturnToMainMenu() // Also used by GameOverScene
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenuScene");
+    }
+
+    ////////////////////////////////////// Temporary buttons
+    public void GameOver()
+    {
+        SceneManager.LoadScene("GameOverScene");
+    }
+
+    public void IncreaseScore()
+    {
+        Score.scoreValue += 100;
+    }
+}
---
+++ SpaceShooterGame/Assets/_Scripts/UI/Score.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+
+public class Score : MonoBehaviour
+{
+    public static int scoreValue = 0;
+    Text scoreText;
+
+    private void Start()
+    {
+        if (SceneManager.GetActiveScene().name == "GameScene") // NOTE: Currently the score cannot be shown in the pause screen as the Score text within the PausePanel will call Start(), resetting the score...
+        {
+            scoreValue = 0;
+        }
+
+        scoreText = GetComponent<Text>();
+    }
+
+    private void Update()
+    {
+        scoreText.text = "Score: " + scoreValue;
+    }
+}
