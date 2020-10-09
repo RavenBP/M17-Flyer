eab2e40bae5e4d701900f4e63b947d4f0baa62a9
RavenBP <ravenbpowless@gmail.com>
2020-10-09 19:03:07 -0400

    added simple player controller
    - added simple player controller with joystick-esque controls
    - added boundaries to GameScene

---
+++ SpaceShooterGame/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 5.0f;
+    private bool touchStart = false;
+    private Vector2 initialTouchPos;
+    private Vector2 currentTouchPos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            initialTouchPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            touchStart = true;
+            currentTouchPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.transform.position.z));
+        }
+        else
+        {
+            touchStart = false;
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        if (touchStart)
+        {
+            Vector2 offset = currentTouchPos - initialTouchPos;
+            Vector2 direction = Vector2.ClampMagnitude(offset, 1.0f);
+            moveCharacter(direction);
+        }
+
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.transform.Translate(direction * speed * Time.deltaTime);
+    }
+}
